54380d132145ba70e2ae74822c17324f7aff767f
Yang <jing.yang2@thermofisher.com>
2020-05-11 10:40:04 +0800

    初始化

---
+++ src/renderer/store/modules/menu.js
@@ -0,0 +1,38 @@
+import { listForRouter } from '@/api/system/menu'
+import { traverseRoutes } from '@/utils/route'
+import { constantRoutes } from '@/router'
+const state = {
+  routes: [],
+  addRoutes: []
+}
+
+const mutations = {
+  SET_ROUTES: (state, routes) => {
+    state.addRoutes = routes
+    state.routes = constantRoutes.concat(routes)
+  }
+}
+
+const actions = {
+
+  getSideMenus({ commit, state }) {
+    return new Promise((resolve, reject) => {
+      listForRouter().then(response => {
+        const menus = response.data
+        console.log('============', menus)
+        const remoteroutes = traverseRoutes(menus)
+        commit('SET_ROUTES', remoteroutes)
+        resolve(remoteroutes)
+      }).catch(error => {
+        reject(error)
+      })
+    })
+  }
+}
+
+export default {
+  namespaced: true,
+  state,
+  mutations,
+  actions
+}
